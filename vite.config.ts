77e25021e285b8583fea2e56a14f31e1f6102e87
SummerJyl <137237074+SummerJyl@users.noreply.github.com>
2025-07-01 12:19:52 -0700

    Updating styling

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5500,
-  },
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    // dev mode
+    return {
+      plugins: [react()],
+      server: {
+        port: 5500,
+      },
+      base: '/', // use root base in dev
+    }
+  } else {
+    // build mode (production)
+    return {
+      plugins: [react()],
+      base: '/Portfolio/', // production base path
+    }
+  }
 })
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 5500,
+//   },
+// })
