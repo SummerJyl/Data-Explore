936aa2a8d17a341badbda702700b546076801c5c
SummerJyl <137237074+SummerJyl@users.noreply.github.com>
2025-07-08 13:28:32 -0700

    Updates, added Ruby

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command }) => {
   if (command === 'serve') {
-    // dev mode
     return {
       plugins: [react()],
       server: {
         port: 5500,
+        strictPort: true, // fail if port 5500 is in use
       },
-      base: '/', // use root base in dev
-    }
+      base: '/',
+    };
   } else {
-    // build mode (production)
     return {
       plugins: [react()],
-      base: '/Portfolio/', // production base path
-    }
+      base: '/Data-Explore/',
+    };
   }
-})
+});
 
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 5500,
-//   },
-// })
